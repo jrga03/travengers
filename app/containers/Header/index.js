2f6549129993b0385b1d46f0948aa5957af771ed
Jason Ray Acido <jasonacido@gmail.com>
2018-10-11 00:28:07 +0800

    created Header

---
+++ app/containers/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { HeaderWrapper } from './styles';
+
+const Header = () => (
+    <HeaderWrapper>
+        <h1>TRAVENGERS</h1>
+    </HeaderWrapper>
+);
+
+export default Header;
---
+++ app/containers/Header/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.nav`
+    width: 100%;
+    height: 75px;
+    background: #2b2b2b;
+    color: #fff;
+
+    h1 {
+        margin: 0;
+    }
+`;
