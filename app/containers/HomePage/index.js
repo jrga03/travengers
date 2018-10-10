e39641d20afd971bb5c915275eeb9f18083b0426
Jason Ray Acido <jasonacido@gmail.com>
2018-10-11 00:28:27 +0800

    initialized HomePage

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,24 +1,37 @@
-/*
- * HomePage
- *
- * This is the first thing users see of our App, at the '/' route
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
- */
+import React, { PureComponent } from 'react';
+import { StyledWrapper } from './styles';
 
-import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
-
-export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
-    );
-  }
+class HomePage extends PureComponent { // eslint-disable-line react/prefer-stateless-function
+    render() {
+        return (
+            <StyledWrapper>
+                <section>
+                    <header>
+                        <h4>Browse Events By Categories</h4>
+                    </header>
+                    <hr />
+                    <div>Categories here</div>
+                    <br />
+                </section>
+                <section>
+                    <header>
+                        <h4>Popular Events</h4>
+                    </header>
+                    <hr />
+                    <div>Popular events here</div>
+                    <br />
+                </section>
+                <section>
+                    <header>
+                        <h4>Events</h4>
+                    </header>
+                    <hr />
+                    <div>Events here</div>
+                    <br />
+                </section>
+            </StyledWrapper>
+        );
+    }
 }
+
+export default HomePage;
---
+++ app/containers/HomePage/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const StyledWrapper = styled.div`
+    h4 {
+        margin: 0;
+    }
+`;
