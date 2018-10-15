384776b4952296be4853670b723b9b11ac40ff4f
Jason Ray Acido <jasonacido@gmail.com>
2018-10-16 00:30:07 +0800

    added header styles

--- app/containers/Header/styles.js
+++ app/containers/Header/styles.js
@@ -5,8 +5,17 @@ export const HeaderWrapper = styled.nav`
     height: 75px;
     background: #2b2b2b;
     color: #fff;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 15px;
 
     h1 {
+        margin: 0;
+    }
+
+    h4 {
         margin: 0;
     }
 `;
