67b76d0b20140535f720ac68efdc1c9a9a56e289
Jason Ray Acido <jasonacido@gmail.com>
2018-10-16 00:30:39 +0800

    starte homepage components and styles

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -13,15 +13,30 @@ class HomePage extends PureComponent { // eslint-disable-line react/prefer-state
                     <div>Categories here</div>
                     <br />
                 </section>
-                <section>
+                <section className="events">
                     <header>
                         <h4>Popular Events</h4>
                     </header>
                     <hr />
-                    <div>Popular events here</div>
+                    <div>
+                        <div className="event">
+                            <img src="https://images.freeimages.com/images/large-previews/478/jack-o-lanterns-1326113.jpg" alt="Test" />
+                            <div>
+                                <div className="event-date">
+                                    <div>01</div>
+                                    <div>Oct</div>
+                                </div>
+                                <h5 className="event-title">Tango for Fast Learners</h5>
+                            </div>
+                            <p>2 The Avenue London W13 8PH UK</p>
+                            <p>Jul 15, 2014 To Jul 15, 2019</p>
+                            <p>3:00 pm To 5:00 pm</p>
+                            <p>(805) 735-2281111</p>
+                        </div>
+                    </div>
                     <br />
                 </section>
-                <section>
+                <section className="events">
                     <header>
                         <h4>Events</h4>
                     </header>
--- app/containers/HomePage/styles.js
+++ app/containers/HomePage/styles.js
@@ -1,7 +1,57 @@
 import styled from 'styled-components';
 
 export const StyledWrapper = styled.div`
+    padding: 15px;
+
     h4 {
         margin: 0;
+    }
+
+    .events {
+        div .event{
+            width: 250px;
+            /* height: 200px; */
+
+            img {
+                width: 100%;
+            }
+
+            div {
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                justify-content: center;
+                text-align: center;
+                margin: 10px 0;
+
+                .event-date {
+                    padding: 0 15px;
+                    margin: 0 10px 10px 0;
+                    border-right-style: solid;
+                    border-right-width: 1px;
+                    border-color: gray;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    justify-content: center;
+
+                    div {
+                        margin: 0;
+                    }
+                }
+
+                .event-title {
+                    text-align: left;
+                    margin: 0;
+                    font-size: 15px;
+                }
+            }
+
+            p {
+                margin: 0;
+                font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+                font-size: 14px;
+            }
+        }
     }
 `;
