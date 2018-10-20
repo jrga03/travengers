efa93771c7c4a84e79b6fdaa4db143311b44ae3c
Jason Ray Acido <jasonacido@gmail.com>
2018-10-20 19:49:47 +0800

    fixed typo

--- app/containers/Header/index.js
+++ app/containers/Header/index.js
@@ -3,7 +3,7 @@ import { HeaderWrapper } from './styles';
 
 const Header = () => (
     <HeaderWrapper>
-        <h1>TRAVENGERS</h1>
+        <h1>TRAVENGER</h1>
         <h4>Login</h4>
     </HeaderWrapper>
 );
