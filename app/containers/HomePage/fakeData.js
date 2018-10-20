fa70127e34bb83ccfcc11f5a1f17a4965350ad78
Jason Ray Acido <jasonacido@gmail.com>
2018-10-20 19:52:06 +0800

    added mockData for events

---
+++ app/containers/HomePage/fakeData.js
@@ -0,0 +1,67 @@
+export const mockEvents = [
+    {
+        id: 0,
+        details: {
+            photo: 'https://images.freeimages.com/images/large-previews/478/jack-o-lanterns-1326113.jpg',
+            dateStart: new Date( '4/1/2018 10:00' ),
+            dateEnd: new Date( '8/31/2018 10:00' ),
+            title: 'Miami Beach Antique Jewelry and Watch Show',
+            location: null,
+            contact: '1234567890'
+        },
+        liked: true,
+        comments: 32
+    },
+    {
+        id: 1,
+        details: {
+            photo: 'https://images.freeimages.com/images/large-previews/0cf/tulips-1-1377350.jpg',
+            dateStart: new Date( '10/20/2018' ),
+            dateEnd: new Date( '10/20/2018' ),
+            title: 'LA Fashion Week',
+            location: 'Fulham Road London SW6 1HS UK',
+            contact: '9123467890'
+        },
+        liked: false,
+        comments: 5
+    },
+    {
+        id: 2,
+        details: {
+            photo: 'https://images.freeimages.com/images/large-previews/8a1/small-waterfall-1376352.jpg',
+            dateStart: new Date( '5/1/2018 20:00' ),
+            dateEnd: new Date( '6/30/2020 22:00' ),
+            title: 'ALL FOR ONE Theatre festival',
+            location: '38 Commerce Street New York, NY 10014',
+            contact: '+86 10 6538 5537'
+        },
+        liked: false,
+        comments: 1
+    },
+    {
+        id: 3,
+        details: {
+            photo: 'https://images.freeimages.com/images/large-previews/223/lighthouse-fish-1311421.jpg',
+            dateStart: new Date( '5/1/2018 10:00' ),
+            dateEnd: new Date( '6/30/2018 10:00' ),
+            title: 'The Great Broadway Sing Along',
+            location: 'San Francisco, CA 94115',
+            contact: null
+        },
+        liked: true,
+        comments: 100
+    },
+    {
+        id: 4,
+        details: {
+            photo: 'https://images.freeimages.com/images/large-previews/9d6/philippine-coins-1483943.jpg',
+            dateStart: new Date( '7/8/2014 12:00' ),
+            dateEnd: new Date( '7/8/2017 14:00' ),
+            title: 'Amazing Amy the Contortionist! With Rachel Mason & Mark Golamco!',
+            location: '39 Essex St New York, NY 10002',
+            contact: '+86 10 6538 5537'
+        },
+        liked: true,
+        comments: 0
+    }
+];
