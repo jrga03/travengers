a6913f974eb661df6be141ce395e110fc229ad86
Jason Ray Acido <jasonacido@gmail.com>
2018-10-27 14:10:40 +0800

    fix indentation and formatting

--- app/app.js
+++ app/app.js
@@ -27,11 +27,11 @@ import { makeSelectLocationState } from 'containers/App/selectors';
 import LanguageProvider from 'containers/LanguageProvider';
 
 // Load the favicon, the manifest.json file and the .htaccess file
-/* eslint-disable import/no-unresolved, import/extensions */
+/* eslint-disable import/no-unresolved, import/extensions, import/no-webpack-loader-syntax */
 import '!file-loader?name=[name].[ext]!./favicon.ico';
 import '!file-loader?name=[name].[ext]!./manifest.json';
 import 'file-loader?name=[name].[ext]!./.htaccess';
-/* eslint-enable import/no-unresolved, import/extensions */
+/* eslint-enable import/no-unresolved, import/extensions, import/no-webpack-loader-syntax */
 
 import configureStore from './store';
 
@@ -49,68 +49,64 @@ import createRoutes from './routes';
 // Optionally, this could be changed to leverage a created history
 // e.g. `const browserHistory = useRouterHistory(createBrowserHistory)();`
 const initialState = {};
-const store = configureStore(initialState, browserHistory);
+const store = configureStore( initialState, browserHistory );
 
 // Sync history and store, as the react-router-redux reducer
 // is under the non-default key ("routing"), selectLocationState
 // must be provided for resolving how to retrieve the "route" in the state
-const history = syncHistoryWithStore(browserHistory, store, {
-  selectLocationState: makeSelectLocationState(),
+const history = syncHistoryWithStore( browserHistory, store, {
+    selectLocationState: makeSelectLocationState()
 });
 
 // Set up the router, wrapping all Routes in the App component
 const rootRoute = {
-  component: App,
-  childRoutes: createRoutes(store),
+    component: App,
+    childRoutes: createRoutes( store )
 };
 
-const render = (messages) => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <LanguageProvider messages={messages}>
-        <Router
-          history={history}
-          routes={rootRoute}
-          render={
-            // Scroll to top when going to a new page, imitating default browser
-            // behaviour
-            applyRouterMiddleware(useScroll())
-          }
-        />
-      </LanguageProvider>
-    </Provider>,
-    document.getElementById('app')
-  );
+const render = ( messages ) => {
+    ReactDOM.render(
+        <Provider store={ store }>
+            <LanguageProvider messages={ messages }>
+                <Router
+                    history={ history }
+                    routes={ rootRoute }
+                    render={ applyRouterMiddleware( useScroll()) }
+                />
+            </LanguageProvider>
+        </Provider>,
+        document.getElementById( 'app' )
+    );
 };
 
 // Hot reloadable translation json files
-if (module.hot) {
-  // modules.hot.accept does not accept dynamic dependencies,
-  // have to be constants at compile-time
-  module.hot.accept('./i18n', () => {
-    render(translationMessages);
-  });
+if ( module.hot ) {
+    // modules.hot.accept does not accept dynamic dependencies,
+    // have to be constants at compile-time
+    module.hot.accept( './i18n', () => {
+        render( translationMessages );
+    });
 }
 
 // Chunked polyfill for browsers without Intl support
-if (!window.Intl) {
-  (new Promise((resolve) => {
-    resolve(import('intl'));
-  }))
+if ( !window.Intl ) {
+    ( new Promise(( resolve ) => {
+        resolve( import( 'intl' ));
+    }))
     .then(() => Promise.all([
-      import('intl/locale-data/jsonp/en.js'),
+        import( 'intl/locale-data/jsonp/en.js' )
     ]))
-    .then(() => render(translationMessages))
-    .catch((err) => {
-      throw err;
+    .then(() => render( translationMessages ))
+    .catch(( err ) => {
+        throw err;
     });
 } else {
-  render(translationMessages);
+    render( translationMessages );
 }
 
 // Install ServiceWorker and AppCache in the end since
 // it's not most important operation and if main code fails,
 // we do not want it installed
-if (process.env.NODE_ENV === 'production') {
-  require('offline-plugin/runtime').install(); // eslint-disable-line global-require
+if ( process.env.NODE_ENV === 'production' ) {
+    require( 'offline-plugin/runtime' ).install(); // eslint-disable-line global-require
 }
